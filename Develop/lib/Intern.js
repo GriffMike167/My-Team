ed075e74a6839d20b6bbaf07aed2b3c8d765018d
Michael Griffith <griffithcmichael@gmail.com>
2020-11-21 22:35:19 -0600

    built class Inturn

--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -1,1 +1,23 @@
 // TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+const Employee = require('./Employee')
+
+class Inturn extends Employee {
+    constructor(name, id, email, github){
+    super (name, id, email)
+    
+    this.github = github;
+    this.role = 'Inturn';
+    }
+
+    getGithub(){
+        return this.github;
+    }
+
+    getRole(){
+        return this.role;
+    }
+
+
+}
+
+module.exports = Intern;
