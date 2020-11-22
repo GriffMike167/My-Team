454a877bed4c7191323f8ee88eb3d2ba563841f4
Michael Griffith <griffithcmichael@gmail.com>
2020-11-21 23:35:30 -0600

    made employee info in node

--- Develop/app.js
+++ Develop/app.js
@@ -1,3 +1,4 @@
+const Employee = require("./lib/Employee");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
@@ -9,6 +10,32 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+
+
+const role = 'Manager';
+const employees = [];
+
+
+const employeeInfor = [
+    {
+        type: "input";
+        name: "name";
+        message: "What is employess name?";
+    },
+    {
+        type: "input";
+        name: "id";
+        message: "Please enter employee id number:";
+    },
+    {
+        type: "input";
+        name: "email";
+        message: "Please enter employee email address: ";
+        
+    }
+]
+
+
 
 
 // Write code to use inquirer to gather information about the development team members,
